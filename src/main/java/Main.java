20f94bbd0ab21d51bdcf642886b000c076152456
munory <gluchova.ka@gmail.com>
2023-07-21 10:39:21 +0500

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(9999)) {
+
+            Socket clientSocket = serverSocket.accept();
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            System.out.println("New connection accepted");
+
+            final String name = in.readLine();
+
+            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+}
